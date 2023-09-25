1bc37c4c0b8ac81d62fd36e912170558d36fbece
William Santana <71521910+williamsantanadeveloper@users.noreply.github.com>
2023-09-25 11:50:15 -0300

    add js

---
+++ Projeto_site_pizzaria/javascript/add_carrinho.js
@@ -0,0 +1,15 @@
+const add_carrinho_pizza_calabresa = () => {
+
+    let result_pizza_calabresa = document.getElementById("result_pizza_calabresa");
+
+    result_pizza_calabresa.textContent = 'Item adicionado';
+
+};
+
+const add_carrinho_pizza_portuguesa = () => {
+
+    let result_pizza_portuguesa = document.getElementById("result_pizza_portuguesa");
+
+    result_pizza_portuguesa.textContent = 'Item adicionado';
+
+};
