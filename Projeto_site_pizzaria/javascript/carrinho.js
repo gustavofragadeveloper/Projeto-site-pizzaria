cea74dc8d20cd65459bed4837e284d1701e0ed48
William Santana <williamandrade1058@gmail.com>
2023-10-30 19:58:27 -0300

    config página do carrinho

--- Projeto_site_pizzaria/javascript/carrinho.js
+++ Projeto_site_pizzaria/javascript/carrinho.js
@@ -3,3 +3,8 @@ function calcular_frete(){
 
     resultado.textContent = "Valor do frete = GRÁTIS"
 }
+
+const calcularPreco = () => {
+    let result = document.getElementById("result");
+    result.textContent = 'O valor é de R$100,00, mas com o desconto R$90,00';
+};
