5bf7be114f517a35fe2536f0c24b7512a9225dd6
William Santana <williamandrade1058@gmail.com>
2023-11-20 22:13:49 -0300

    configurando cardapio

--- Projeto_site_pizzaria/javascript/add_carrinho.js
+++ Projeto_site_pizzaria/javascript/add_carrinho.js
@@ -1,15 +1,45 @@
 const add_carrinho_pizza_calabresa = () => {
 
-    let result_pizza_calabresa = document.getElementById("result_pizza_calabresa");
+    let result_pizza_calabresa = document.getElementById("msg_pizza_calabresa");
 
     result_pizza_calabresa.textContent = 'Item adicionado';
 
 };
 
-const add_carrinho_pizza_portuguesa = () => {
+const add_carrinho_pizza_napolitano = () => {
 
-    let result_pizza_portuguesa = document.getElementById("result_pizza_portuguesa");
+    let result_pizza_napolitano = document.getElementById("result_pizza_napolitano");
 
-    result_pizza_portuguesa.textContent = 'Item adicionado';
+    result_pizza_napolitano.textContent = 'Item adicionado';
 
+};
+
+const add_carrinho_pizza_bacon_turbinado = () => {
+
+    let result_pizza_bacon_turbinado = document.getElementById("result_pizza_bacon_turbinado");
+
+    result_pizza_bacon_turbinado.textContent = 'Item adicionado';
+
+};
+
+const add_carrinho_pizza_4_queijos = () => {
+
+    let result_pizza_4_queijos = document.getElementById("result_pizza_4_queijos");
+
+    result_pizza_4_queijos.textContent = 'Item adicionado';
+
+};
+
+const add_carrinho_pizza_americana = () => {
+
+    let result_pizza_americana = document.getElementById("result_pizza_americana");
+
+    result_pizza_americana.textContent = 'Item adicionado';
+}
+
+const add_carrinho_pizza_vegetariana = () => {
+
+    let result_pizza_vegetariana = document.getElementById("result_pizza_vegetariana");
+
+    result_pizza_vegetariana.textContent = 'Item adicionado';
 };
